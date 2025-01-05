675693c9bbdf506e783576f2f28c6d44466fd07a
Sao-Sothea <sothea.sao.15@gmail.com>
2025-01-05 17:08:46 +0700

    first commit

---
+++ Assigment1/introduction.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "<h1 >Introduction Myself</h1>";
+    echo "<h3>Name: Sao Sothea</h3>";
+    echo "<h3>Gender: Male</h3>";
+    echo "<h3>University: RUPP</h3>";
+    echo "<h3>Year: 2</h3>";
+    echo "<h3>Major: IT</h3>";
+    echo "<h3>Phone number: 090 42 66 32</h3>";
+    echo "<h3>Email: sothea.sao.15@gmail.com</h3>";
+    ?>
+</body>
+</html>
